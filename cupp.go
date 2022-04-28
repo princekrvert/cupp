870e44e70d7d88b4ea0900859a3b4201875ffcf9
PRINCE KUMAR <56459297+princekrvert@users.noreply.github.com>
2022-04-28 10:08:25 +0530

    Add files via upload
    first commit in cupp

---
+++ cupp.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+/// store all the password in the data slice
+//var data []string
+// create a help function ---
+func help() {
+	fmt.Println("\033[35;1m Use : cupp ")
+	fmt.Println("\033[35;1m Cupp is a password generator for the indian user ")
+	fmt.Println("\033[35;1m Argument : -h or --help for the help ")
+	fmt.Println("\033[35;1m Argument : -i for intractive mode ")
+}
+func main() {
+	if os.Args[1] == "-h" || os.Args[1] == "--help" {
+		help()
+	} else if os.Args[1] == "-i" || os.Args[1] == "-i " {
+		// now take the input from the user and store them
+		var fName, lName, dob, aadhar, num string
+		var userData []string
+		fmt.Print("\033[33;1m First name of the target: ")
+		fmt.Scanf("%s", &fName)
+		fmt.Print("\033[33;1m Last name of the target: ")
+		fmt.Scanf("%s", &lName)
+		fmt.Print("\033[33;1m Date of birth: ")
+		fmt.Scanf("%s", &dob)
+		fmt.Print("\033[33;1m Target mobile number without 91 :")
+		fmt.Scanf("%s", &num)
+		fmt.Print("\033[33;1m First four digit aadhar number: ")
+		fmt.Scanf("%s", &aadhar)
+		userData = append(userData, fName, lName, dob, aadhar, num)
+		fmt.Print(userData)
+
+	} else {
+		fmt.Println("\033[31;1m Invalid argument provided ")
+	}
+}
