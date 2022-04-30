8b198551dfd5637c323ea2e32477af780cff7a8f
PRINCE KUMAR <56459297+princekrvert@users.noreply.github.com>
2022-04-30 08:41:00 +0530

    Add files via upload
    third commit in cupp

--- cupp.go
+++ cupp.go
@@ -9,8 +9,9 @@ import (
 /// store all the password in the data slice
 //var data []string
 // create the @ function
-func attherate(uData []string) {
+func underAtthe(uData []string) {
 	var pass []string
+	var years = []string{"1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022"}
 	fName := uData[0]
 	for _, data := range uData {
 		if data == fName {
@@ -18,6 +19,8 @@ func attherate(uData []string) {
 		} else {
 			password := fName + "@" + data
 			pass = append(pass, password)
+			passunder := fName + "_" + data
+			pass = append(pass, passunder)
 		}
 	}
 	fLatterU := strings.ToUpper(string(fName[0]))
@@ -28,12 +31,23 @@ func attherate(uData []string) {
 		} else {
 			password := fNameU + "@" + data
 			pass = append(pass, password)
+			passunder := fNameU + "_" + data
+			pass = append(pass, passunder)
 		}
+	}
+	for _, year := range years {
+		password := fName + "@" + year
+		pass = append(pass, password)
+		passunder := fName + "_" + year
+		pass = append(pass, passunder)
+		passwords := fNameU + "@" + year //second password for the years
+		pass = append(pass, passwords)
+		passunders := fNameU + "_" + year
+		pass = append(pass, passunders)
 	}
 	fmt.Print(pass)
 
-}
-
+} //create a special function --
 // create a help function ---
 func help() {
 	fmt.Println("\033[35;1m Use : cupp ")
@@ -59,7 +73,7 @@ func main() {
 		fmt.Print("\033[33;1m First four digit aadhar number: ")
 		fmt.Scanf("%s", &aadhar)
 		userData = append(userData, fName, lName, dob, aadhar, num)
-		attherate(userData)
+		underAtthe(userData)
 
 	} else {
 		fmt.Println("\033[31;1m Invalid argument provided ")
